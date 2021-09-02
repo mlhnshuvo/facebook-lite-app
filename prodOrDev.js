571e923f41a5b00ebd08c0ece0d4038fb07365b9
Mahamudul Hasan Shuvo <mlhnshuvo@gmail.com>
2021-09-02 23:20:45 +0600

    env add

--- prodOrDev.js
+++ prodOrDev.js
@@ -4,17 +4,16 @@ let ReCAPTCHA_SECRET_KEY
 let FRONTEND_URL
 
 if (process.env.NODE_ENV === 'production') {
-    MONGODB_URL = `mongodb+srv://mlhnshuvo:${process.env.MONGODB_PASSWORD}@cluster.vg1yn.mongodb.net/facebook-lite-app-mhs?retryWrites=true&w=majority`
+    MONGODB_URL = process.env.MONGODB_PASSWORD
     ReCAPTCHA_SITE_KEY = process.env.ReCAPTCHA_SITE_KEY_PROD
     ReCAPTCHA_SECRET_KEY = process.env.ReCAPTCHA_SECRET_KEY_PROD
     FRONTEND_URL = 'https://facebook-lite-app-mhs.herokuapp.com'
 } else {
-    MONGODB_URL = `mongodb+srv://mlhnshuvo:${process.env.MONGODB_PASSWORD}@cluster.vg1yn.mongodb.net/facebook-lite-app-mhs?retryWrites=true&w=majority`
+    MONGODB_URL = process.env.MONGODB_PASSWORD
     ReCAPTCHA_SITE_KEY = process.env.ReCAPTCHA_SITE_KEY
     ReCAPTCHA_SECRET_KEY = process.env.ReCAPTCHA_SECRET_KEY
     FRONTEND_URL = 'http://localhost:3000'
 }
-console.log(process.env.MONGODB_PASSWORD);
 
 module.exports = {
     MONGODB_URL,
