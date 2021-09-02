156c79ebfcc5b482c7607ded598aab1bbe3ef772
Mahamudul Hasan Shuvo <mlhnshuvo@gmail.com>
2021-09-02 21:13:00 +0600

    tring to fix

--- client/src/App.cofig.js
+++ client/src/App.cofig.js
@@ -4,7 +4,7 @@ const AppConfig = () => {
 
     if (process.env.NODE_ENV === 'production') {
         ReCAPTCHA_SITE_KEY = '6LdjlocbAAAAABWpq-813GMixkCv27OBlcdWJi3F'
-        FRONTEND_URL = 'https://mern-facebook-lite-mhs.herokuapp.com'
+        FRONTEND_URL = 'https://facebook-lite-app-mhs.herokuapp.com'
     } else {
         ReCAPTCHA_SITE_KEY = '6LfXYIYbAAAAAMubzTBtocBF3-ynGKm4FvlvTPB7'
         FRONTEND_URL = `http://localhost:3000`
